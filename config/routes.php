c20968dabd02d8b21bab4510226e2272fd90aba7
Kenshin38 <makramourguedi@gmail.com>
2020-02-10 15:59:18 +0100

    FirstROuteSrrviceContainer

--- config/routes.php
+++ config/routes.php
@@ -1,14 +1,9 @@
 <?php
 
-use Bramus\Router\Router;
+$router = $container->getRouter();
 
-require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/routes.php';
-?>
-<?php
-$router = new Router;
-$router->get('/hello', function () {
-    echo "Hello world !";
+$router->get('/zboub', function () {
+    echo "Stella Chapon";
 });
 $router->run();
 ?>
